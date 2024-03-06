414157564b2b83b165d9c1bb7dfc48e1d3b1131a
adamd <adam.d.hinton@gmail.com>
2024-03-06 11:55:19 -0500

    Example without generics, now to refactor as generic

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,83 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+record BaseballPlayer(String name, String position){
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros = new BaseballTeam("Houston Astros");
+
+//        scoreResult(phillies, 3, astros, 5);
+
+        var harper = new BaseballPlayer("B Harper", "Right Fielder");
+        var marsh = new BaseballPlayer("B Marsh", "Right Fielder");
+
+        phillies.addTeamMember(harper);
+        phillies.addTeamMember(marsh);
+
+        phillies.listTeamMembers();
+
+    }
+
+    public static void scoreResult (BaseballTeam team1, int t1_score, BaseballTeam team2, int t2_score){
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+}
+
+class BaseballTeam{
+
+    private String teamName;
+    private List<BaseballPlayer> teamMembers = new ArrayList<>();
+    private int totalWins = 0;
+    private int totalLosses = 0;
+    private int totalTies = 0;
+
+    public BaseballTeam(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public void addTeamMember(BaseballPlayer player){
+        if (!teamMembers.contains(player)) {
+            teamMembers.add(player);
         }
+
+    }
+
+    public void listTeamMembers() {
+        System.out.println(teamName + "Roster:");
+        System.out.println(teamMembers);
+    }
+
+    public int ranking() {
+        return (totalLosses * 2) + (totalTies + 1);
+    }
+
+    public String setScore(int ourScore, int theirScore){
+        String message = "lost to";
+        if (ourScore > theirScore){
+            totalWins++;
+            message = "beat";
+        }
+        else if (ourScore == theirScore){
+            totalTies++;
+            message = "tied";
+        }
+        else{
+            totalLosses++;
+        }
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return teamName + " (Ranked " + ranking() + ")";
     }
 }
